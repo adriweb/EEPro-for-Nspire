04f1f9cb2d140bb69439c9bf6c3c5d1dbd64c714
Adrien Bertrand <adrienbertrand@msn.com>
2011-11-29 01:32:47 +0100

    Greek Alphabet part done. TODO : Make it compatible with the rest by using it with Screen()

---
+++ Reference Part/REFGreekAlphabet.lua
@@ -0,0 +1,44 @@
+alphabet1 = {
+ { "Α", "α", "Alpha" },
+ { "Β", "β", "Beta" },
+ { "Γ", "γ", "Gamma" },
+ { "Δ", "δ", "Delta" },
+ { "Ε", "ε", "Epsilon" },
+ { "Ζ", "ζ", "Zeta" },
+ { "Η", "η", "Eta" },
+ { "Θ", "θ", "Theta" }
+}
+alphabet2 = {
+ { "Ι", "ι", "Iota" },
+ { "Κ", "κ", "Kappa" },
+ { "Λ", "λ", "Lambda" },
+ { "Μ", "μ", "Mu" },
+ { "Ν", "ν", "Nu" },
+ { "Ξ", "ξ", "Xi" },
+ { "Ο", "ο", "Omicron" },
+ { "Π", "π", "Pi" }
+}
+alphabet3 = {
+ { "Ρ", "ρ", "Rho" },
+ { "Σ", "σ", "Sigma" },
+ { "Τ", "τ", "Tau" },
+ { "Υ", "υ", "Upsilon" },
+ { "Φ", "φ", "Phi" },
+ { "Χ", "χ", "Chi" },
+ { "Ψ", "ψ", "Psi" },
+ { "Ω", "ω", "Omega" }
+}
+
+function on.paint(gc)
+	msg = "Greek Alphabet"
+	gc:drawString(msg,.5*(platform.window:width()-gc:getStringWidth(msg)),5,"top")
+	for k,v in pairs(alphabet1) do
+		gc:drawString(v[3] .. " : " .. v[1] .. " / " .. v[2], 5, 10+22*k, "top")
+	end
+	for k,v in pairs(alphabet2) do
+		gc:drawString(v[3] .. " : " .. v[1] .. " / " .. v[2], 5+.34*platform.window:width(), 10+22*k, "top")
+	end
+	for k,v in pairs(alphabet3) do
+		gc:drawString(v[3] .. " : " .. v[1] .. " / " .. v[2], 5+.67*platform.window:width(), 10+22*k, "top")
+	end
+end
