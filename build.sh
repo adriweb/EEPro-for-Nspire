2639562f08e0f6c0366f1d00f28977e3dc273031
Adrien Bertrand <adrienbertrand@msn.com>
2011-12-09 00:43:24 +0100

    Updated the script that Jim didn't fix :P

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+# Jim and Adriweb
+# Building EEProNspire....
 
-echo "Building EEPro"
+echo "Building EEProNspire..."
 
 
-cd ..
-cd 1\ -\ Analysis\ Part
-./build.sh
-
-cd 2\ -\ FormulaPro
+cd 1\ -\ \ Analysis\ Part
 ./build.sh
 
 cd ..
-cd 3\ -\ Reference\ Part
+cd 2\ -\ \ FormulaPro
+./build.sh
+
+cd ..
+cd 3\ -\ \ Reference\ Part
 ./build.sh
 
 cd ..
@@ -19,7 +21,6 @@ cd Global\ Libraries
 ./build.sh
 
 cd ..
-
 
 cat lib.big.lua FormulaPro.big.lua Analysis.big.lua Reference.big.lua main.lua > EEPro.big.lua
 luna EEPro.big.lua EEPro.tns
