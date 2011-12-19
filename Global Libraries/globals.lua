c9926abc2542ec4b7b3c6c7367bb672dfdf2bdb7
Adrien Bertrand <adrienbertrand@msn.com>
2011-12-19 22:05:09 +0100

    Added BetterLuaAPI to the globals.lua
    maybe the functions need to go somewhere else ?

--- Global Libraries/globals.lua
+++ Global Libraries/globals.lua
@@ -1,6 +1,9 @@
 ------------------------------------------------------------------
 --                  Overall Global Variables                    --
 ------------------------------------------------------------------
+--
+-- Uses BetterLuaAPI : https://github.com/adriweb/BetterLuaAPI-for-TI-Nspire
+--
 
 a_acute = string.uchar(225)
 a_circ  = string.uchar(226)
@@ -17,3 +20,145 @@ omega = string.uchar(2126)
 sup_plus = string.uchar(8314)
 sup_minus = string.uchar(8315)
 right_arrow = string.uchar(8594)
+
+
+Color = {
+	["black"] = {0, 0, 0},
+	["red"] = {0xff, 0, 0},
+	["green"] = {0, 0xff, 0},
+	["blue "] = {0, 0, 0xff},
+	["white"] = {0xff, 0xff, 0xff},
+	["brown"] = {165,42,42},
+	["cyan"] = {0,255,255},
+	["darkblue"] = {0,0,139},
+	["darkred"] = {139,0,0},
+	["fuchsia"] = {255,0,255},
+	["gold"] = {255,215,0},
+	["gray"] = {127,127,127},
+	["grey"] = {127,127,127},
+	["lightblue"] = {173,216,230},
+	["lightgreen"] = {144,238,144},
+	["magenta"] = {255,0,255},
+	["maroon"] = {128,0,0},
+	["navyblue"] = {159,175,223},
+	["orange"] = {255,165,0},
+	["palegreen"] = {152,251,152},
+	["pink"] = {255,192,203},
+	["purple"] = {128,0,128},
+	["royalblue"] = {65,105,225},
+	["salmon"] = {250,128,114},
+	["seagreen"] = {46,139,87},
+	["silver"] = {192,192,192},
+	["turquoise"] = {64,224,208},
+	["violet"] = {238,130,238},
+	["yellow"] = {255,255,0}
+}
+Color.mt = {__index = function () return {0,0,0} end}
+setmetatable(Color,Color.mt)
+
+function copyTable(t)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
+function deepcopy(t) -- This function recursively copies a table's contents, and ensures that metatables are preserved. That is, it will correctly clone a pure Lua object.
+	if type(t) ~= 'table' then return t end
+	local mt = getmetatable(t)
+	local res = {}
+	for k,v in pairs(t) do
+		if type(v) == 'table' then
+		v = deepcopy(v)
+		end
+	res[k] = v
+	end
+	setmetatable(res,mt)
+	return res
+end -- from http://snippets.luacode.org/snippets/Deep_copy_of_a_Lua_Table_2
+
+function test(arg)
+	return arg and 1 or 0
+end
+
+function screenRefresh()
+	return platform.window:invalidate()
+end
+
+function pww()
+	return platform.window:width()
+end
+
+function pwh()
+	return platform.window:height()
+end
+
+function drawPoint(gc, x, y)
+	gc:fillRect(x, y, 1, 1)
+end
+
+function drawCircle(gc, x, y, diameter)
+	gc:drawArc(x - diameter/2, y - diameter/2, diameter,diameter,0,360)
+end
+
+function drawCenteredString(gc, str)
+	gc:drawString(str, (pww() - myGC:getStringWidth(str)) / 2, pwh() / 2, "middle")
+end
+
+function drawXCenteredString(gc, str,y)
+	gc:drawString(str, (pww() - myGC:getStringWidth(str)) / 2, y, "top")
+end
+
+function setColor(gc,theColor)
+	if type(theColor) == "string" then
+		theColor = string.lower(theColor)
+		if type(Color[theColor]) == "table" then gc:setColorRGB(unpack(Color[theColor])) end
+	elseif type(theColor) == "table" then
+		myGC:setColorRGB(unpack(theColor))
+	end
+end
+
+function verticalBar(gc,x)
+	gc:fillRect(gc,x,0,1,pwh())
+end
+
+function horizontalBar(gc,y)
+	gc:fillRect(gc,0,y,pww(),1)
+end
+
+function drawSquare(gc,x,y,l)
+	drawPolyLine(gc,{(x-l/2),(y-l/2), (x+l/2),(y-l/2), (x+l/2),(y+l/2), (x-l/2),(y+l/2), (x-l/2),(y-l/2)})
+end
+
+function drawRoundRect(gc,x,y,wd,ht,rd)  -- wd = width, ht = height, rd = radius of the rounded corner
+	x = x-wd/2  -- let the center of the square be the origin (x coord)
+	y = y-ht/2 -- same for y coord
+	if rd > ht/2 then rd = ht/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max rd)
+	gc:drawLine(x + rd, y, x + wd - (rd), y);
+	gc:drawArc(x + wd - (rd*2), y + ht - (rd*2), rd*2, rd*2, 270, 90);
+	gc:drawLine(x + wd, y + rd, x + wd, y + ht - (rd));
+	gc:drawArc(x + wd - (rd*2), y, rd*2, rd*2,0,90);
+	gc:drawLine(x + wd - (rd), y + ht, x + rd, y + ht);
+	gc:drawArc(x, y, rd*2, rd*2, 90, 90);
+	gc:drawLine(x, y + ht - (rd), x, y + rd);
+	gc:drawArc(x, y + ht - (rd*2), rd*2, rd*2, 180, 90);
+end
+
+function fillRoundRect(gc,x,y,wd,ht,radius)  -- wd = width and ht = height -- renders badly when transparency (alpha) is not at maximum >< will re-code later
+	if radius > ht/2 then radius = ht/2 end -- avoid drawing cool but unexpected shapes. This will draw a circle (max radius)
+    gc:fillPolygon({(x-wd/2),(y-ht/2+radius), (x+wd/2),(y-ht/2+radius), (x+wd/2),(y+ht/2-radius), (x-wd/2),(y+ht/2-radius), (x-wd/2),(y-ht/2+radius)})
+    gc:fillPolygon({(x-wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y-ht/2), (x+wd/2-radius+1),(y+ht/2), (x-wd/2+radius),(y+ht/2), (x-wd/2+radius),(y-ht/2)})
+    x = x-wd/2  -- let the center of the square be the origin (x coord)
+	y = y-ht/2 -- same
+	gc:fillArc(x + wd - (radius*2), y + ht - (radius*2), radius*2, radius*2, 1, -91);
+    gc:fillArc(x + wd - (radius*2), y, radius*2, radius*2,-2,91);
+    gc:fillArc(x, y, radius*2, radius*2, 85, 95);
+    gc:fillArc(x, y + ht - (radius*2), radius*2, radius*2, 180, 95);
+end
+
+function drawLinearGradient(color1,color2)
+	-- syntax would be : color1 and color2 as {r,g,b}.
+ 	-- don't really know how to do that. probably converting to hue/saturation/light mode and change the hue.
+ 	-- todo with unpack(color1) and unpack(color2)
+end
