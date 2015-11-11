b5680745bdb9fead04733f6b8817fc04808fce9f
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-11-11 17:31:57 -0500

    Cleaner build script

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Jim and Adriweb
-# Building FormulaPro Nspire v1.41b....
 
-echo "Building FormulaPro Nspire v1.41b..."
+echo "Building FormulaPro Nspire v1.42b..."
+
 echo "Building the database"
-
 cd 0\ -\ \ Database
 ./build.sh
 
@@ -33,18 +32,13 @@ cd ..
 echo "Creating the whole thing..."
 #cat Database.big.lua lib.big.lua FormulaPro.big.lua Analysis.big.lua Reference.big.lua main.lua > EEPro.big.lua
 cat Database.big.lua lib.big.lua FormulaPro.big.lua Reference.big.lua main.lua > EEPro.big.lua
-luna EEPro.big.lua EEPro.tns
-cp EEPro.tns FormulaPro.tns
+mv EEPro.big.lua FormulaPro_fullcode.lua
+luna FormulaPro_fullcode.lua FormulaPro.tns
+cp FormulaPro.tns EEPro.tns
 
 echo "Done building"
-echo "Cleaning up"
 
-rm lib.big.lua
-rm FormulaPro.big.lua
-#rm Analysis.big.lua
-rm Reference.big.lua
-rm Database.big.lua
-#rm EEPro.big.lua
+echo "Cleaning up"
+rm *.big.lua
 
 echo "Done. Enjoy !"
-
